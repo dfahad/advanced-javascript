f695a6400ecf43e13d359d71c5c9749a6986a158
dfahad <rezoun.fahad@gmail.com>
2020-03-11 21:54:02 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,42 @@
+let pakhi;
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const result = add(13);
+console.log(result);
+
+
+const premik = {name:"smart dude", phone:458123};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [2, 5, 9];
+console.log(ages[3]);
+
+
+/**
+ * why show undefined?
+ * if not set value in var/let/const;
+ * if not use return;
+ * if written return, but not written with name;
+ * if equal argument not pass accordingly to parameter number;
+ * if object has no property;
+ * if undefined word use;
+ * if absent index number console of array;
+ * 
+ * why use null?
+ * only developer use null for showing null;
+ */
