8c16ecadf3b6b95997477f1320c476c339851c8d
dfahad <rezoun.fahad@gmail.com>
2020-03-11 20:12:17 +0600

    true false condition

---
+++ true-false.js
@@ -0,0 +1,21 @@
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+/**
+ * for number (+ve/-ve) value == true;
+ * but for 0 == false;
+ * for string with something == true;
+ * but string "" == false;
+ * but string " " == true;
+ * for undefined == false;
+ * for NaN (not a number) == false;
+ * for null == false;
+ * for empty array [] == true;
+ * without property object {} == true;
+ * false == false;
+ * but 'false' == true; 
+ */
